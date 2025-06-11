4f059cf4ba7ee9f77d8a7251020e70f6261860aa
uxue-sudupe <u-sudupe@eustat.eus>
2025-06-11 10:36:09 +0000

    Deploying to gh-pages from  @ 5c32873e853504d9cfdfe02d0d09ee7256522e44 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3652,7 +3652,7 @@ function updateHeadlineColor(contrast, chartInfo) {
  * @return {String} The headline color in hex form.
  */
 function getHeadlineColor(contrast) {
-    return isHighContrast(contrast) ? '#55a6e5' : '#004466';
+    return isHighContrast(contrast) ? '#55a6e5' : '#e5243b#dda63a#4c9f38#c5192d#ff3a21#26bde2#fcc30b#a21942#fd6925#dd1367#fd9d24#bf8b2e#3f7e44#0a97d9#56c02b#00689d#19486a';
 }
 
 /**
